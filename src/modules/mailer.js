a3042d5ab15b13fbb7ca54f6242ee8973e901bc8
itsyoboygod <m.vini.ac@hotmail.com>
2021-03-19 16:02:20 -0300

    from igloo to home

---
+++ src/modules/mailer.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+const path = require("path");
+
+const hbs = require("nodemailer-express-handlebars");
+
+const { host, port, user, pass } = require("../config/mailer.json");
+
+const transport = nodemailer.createTransport({
+    host,
+    port,
+    auth: { user, pass },
+});
+
+transport.use(
+    "compile",
+    hbs({
+        viewEngine: "handlebars",
+        viewPath: path.resolve("./src/resources/mail"),
+        extName: ".html",
+    })
+);
+
+module.exports = transport;
