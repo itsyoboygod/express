f126491bca8e325c8c22836fcc77a473aac35a0b
itsyoboygod <m.vini.ac@hotmail.com>
2021-04-07 21:32:33 -0300

    some changes in projectController && models.js added

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,13 +1,37 @@
 const express = require("express");
-
 const authMidlleware = require("../midllewares/auth");
+
+const Project = require('../models/projects')
+const Task = require('../models/task')
+
 
 const router = express.Router();
 
 router.use(authMidlleware);
 
-router.get("/", (req, res) => {
-    res.send({ ok: true });
+router.get("/", async (req, res) => {
+    res.send({ user: req.userId });
 });
 
-module.exports = (app) => app.use("/projects", router);
+router.get('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+
+});
+
+router.get('/', async (req, res) => {
+    res.send({ user: req.userId });
+
+});
+
+router.put('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+    
+router.delete('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+    
+    
+    
+    module.exports = (app) => app.use("/projects", router);
